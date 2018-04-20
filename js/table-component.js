bf76a650ea078447fdc5329a6fd481dbc327c9d8
douglaskaryon <douglas@karyon.com.br>
2018-04-20 19:57:45 -0300

    continuacao table

--- js/table-component.js
+++ js/table-component.js
@@ -6,15 +6,46 @@
     });
 
     function criaFiltros(){
-        const   el = $(this),
-                coluna = el.find('.filter-row th');
-        
-        coluna.each(function(){
-            console.log(this);
+        const tabela = $(this),
+        rowFiltros = tabela.find('.filter-row th');
+    
+        rowFiltros.each(function(){
+            const colFiltro = $(this),
+                   inputText = colFiltro.children('input[type="text"]'),
+                   select = colFiltro.children('input[type="text"]'),
+                   checkboxGroup = colFiltro.children('input[type="text"]');
+
+            if (colFiltro.html() !== ""){
+                if (inputText.length > 0){
+                    colFiltro.on('keyup', filtrar);
+                    //console.log(colFiltro.parent().children().index());
+                };
+                if (select.length > 0){
+                    select.on('change', filtrar);
+                    // tabela.find('tbody tr').each(function(){
+                    //     const dado = $(this).children('td').eq(select.parent()[0].cellIndex).text();
+                    //     select.append("<option value='" + dado + "'>" + dado + "</option>");
+                    // })
+                    
+                    // select.map(function(){
+                    //     const txtColuna = $(this).children('td').eq(select.parent()[0].cellIndex).text();
+                        
+                    //     if (select.find(':contains(' + txtColuna + ')').length === 0){
+                    //         select.append("<span class='filtro-situacao'><input id='id"+ txtColuna +"' type='checkbox'><label for='id"+ txtColuna +"'>"+ txtColuna +"</label></span>");
+                    //     }   
+                    // });
+
+                };
+                if (checkboxGroup.length > 0){
+                    //console.log(colFiltro.parent().children().index());
+                };
+            }
         });
         
-
     };
 
+    function filtrar(){
+        console.log('filtrando!');
+    }
 
 })();
