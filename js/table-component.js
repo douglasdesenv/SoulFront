99f25eb4088b15e672d8cbd596face8033b5e0e1
douglaskaryon <douglas@karyon.com.br>
2018-05-09 13:38:54 -0300

    table - função filtrar

--- js/table-component.js
+++ js/table-component.js
@@ -18,10 +18,8 @@
             if (colFiltro.html() !== ""){
                 if (inputText.length > 0){
                     inputText.on('keyup', filtrar);
-                };
-                if (select.length > 0){
-                    select.on('change', filtrar);
-                    
+
+                } else{
                     let colIndex = i+1;
                     const tdsColuna = tabela.find('tbody td:nth-child('+ colIndex +')');
 
@@ -29,57 +27,53 @@
                         return $(this).text();
                     }).get();
 
-                    $.unique(textosColuna).map(function(el){
-                        select.append("<option value='" + el + "'>" + el + "</option>");
-                    });
-
-                };
-                if (checkboxGroup.length > 0){
-                    
-                    checkboxGroup.each(function(){
-                        $(this).on('click', function(){
-                            if (checkboxList.is(':hidden')) {
-                                checkboxList.css('display','grid');  
-                            } else {
-                                checkboxList.hide();
-                            }
-                        });
-
-                        const checkboxList = $(this).children('.checkbox-list');
-
-                        let colIndex1 = i+1;
-                        const tdsColuna1 = tabela.find('tbody td:nth-child('+ colIndex1 +')');
+                    if (select.length > 0){
+                        select.on('change', filtrar);
                         
-                        let textosColuna1 = tdsColuna1.map(function(){
-                            return $(this).text();
-                        }).get();
-
-                        $.unique(textosColuna1).map(function(el){
-                            checkboxList.append('<input id="id'+ el +'" type="checkbox"><label for="id'+ el + '">' + el + '</label>');
+                        $.unique(textosColuna).map(function(el){
+                            select.append("<option value='" + el + "'>" + el + "</option>");
                         });
-  
-                    })
-                };
+
+                    } else {
+                        if (checkboxGroup.length > 0){                                
+                            checkboxGroup.each(function(){
+                                const checkboxList = $(this).children('.checkbox-list');
+                                
+                                $(this).on('click', function(){
+                                    if (checkboxList.is(':hidden')) {
+                                        checkboxList.css('display','grid');  
+                                    } else {
+                                        checkboxList.hide();
+                                    }
+                                });
+    
+                                $.unique(textosColuna).map(function(el){
+                                    checkboxList.append('<input id="id'+ el +'" type="checkbox"><label for="id'+ el + '">' + el + '</label>');
+                                });
+                                checkboxList.children('input[type="checkbox"]').on('change', filtrar);
+                            })
+                        };
+                    }
+                }          
             }
         });
-        
-    };
-    
+    };   
 
     function filtrar(){
         const el = $(this);
-
-        if (el.prop('tagName') == "INPUT"){
-            console.log('é input')
-        };
-        if (el.prop('tagName') == "SELECT"){
-            console.log(el)
-            const opcaoFiltro = el.val();
-            
-        };
-        if (el.hasClass('checkbox-group')){
-            console.log('é checkbox-group')
-        };
+        console.log(el);
+        if (el.prop('type') == "text"){
+            console.log(tdsColuna);
+            //$(':not(:contains(' + $(this).val() + '))').parent('tr').hide();
+        } else{
+            if (el.prop('type') == "select-one"){
+                console.log('é select')
+            } else {
+                if (el.prop('type') == "checkbox"){
+                    console.log('é checkbox')
+                };
+            }
+        };  
     }
 
 })();
