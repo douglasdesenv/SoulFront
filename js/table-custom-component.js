7a0ea160f92f1857b7e419e810d8b82d99088631
douglaskaryon <douglas@karyon.com.br>
2018-09-28 18:56:09 -0300

    Componente Table custom finalizado

--- js/table-custom-component.js
+++ js/table-custom-component.js
@@ -7,23 +7,49 @@
 
     function carregaCustomizacao(){
         $('[data-table-custom]').each(function(){
-            $(this).on("click", function() {clicarMenuColunas($(this))});   
+            clicarMenuColunas($(this));   
         });
         modTabela.carregaTabela();
     }
 
     function clicarMenuColunas(el){
-        const   tabela = el.closest('table'),
-                titleRow = tabela.find('.title-row th');
-                console.log(titleRow);
+        const   table = $(el.data('table-custom')),
+                menuColumn = el.next('.menu-column'),
+                titleRow = table.find('tr th:not([data-table-custom-col="desindexed"])');
+                
 
-                titleRow.each(function(){
-                    let $this = $(this);
-                    $('.menu-column').append('<input id="id'+ $this.text() +'" type="checkbox"><label for="id'+ $this.text() + '">' + $this.text() + '</label>');
+        titleRow.each(function(){
+            const $this = $(this);
+            
+            $('.menu-column').append('<input id="id'+ $this.text() +'" type="checkbox" checked><label for="id'+ $this.text() + '">' + $this.text() + '</label>');
+            
+            const checkBox = $("#id" + $this.text());
+            
+            checkBox.on("change", function() { 
+                $this.closest('table').find('td:nth-child('+ ($this[0].cellIndex + 1) +'), th:nth-child('+ ($this[0].cellIndex + 1) +')').toggle(); 
+            }); 
+
+            if ($this.data('table-custom-col') === 'hidden'){
+                checkBox.change().removeAttr('checked');
+            }
+        });
+
+        if (table.data('data-table-custom-fixed')) {
+            table.css('width', table.outerWidth());
+        };
+
+        $('.btn-column').off('click').on('click', function(){
+            if (menuColumn.is(':hidden')) {
+                menuColumn.css('display','grid');
+                $(document).off('click').on('click', function(e){
+                    if ((menuColumn.is(':visible')) && ($(e.target).closest('.column-choice-group').length == 0)) {
+                        menuColumn.hide();
+                    }
                 });
-        
-        console.log(titleRow);
-        console.log(tabela);
+            } else {
+                menuColumn.hide();
+            }
+        });
     }
 
 })(modTabela);
